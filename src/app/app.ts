131dce9233ec4e5bafaa27e14da34f4f09e6f760
gsvsantos <gustavocnsantos02@gmail.com>
2025-09-24 14:40:48 -0300

    feat: Implementa componente de pokemon-list.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,55 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Pokemon } from './models/pokemon';
-import { NgClass } from "@angular/common";
-import { colorByTypeMapping } from './utils/color-by-type-mapping';
+import { Component } from '@angular/core';
+import { PokemonList } from './components/pokemon-list/pokemon-list';
 
 @Component({
   selector: 'app-root',
-  imports: [NgClass],
+  imports: [PokemonList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements OnInit {
-  public pokemons: Pokemon[] = [];
-  public colorByTypeMapping = colorByTypeMapping;
-  public theBest: string = "";
-  private readonly url: string = "https://pokeapi.co/api/v2/pokemon/?limit=10";
-  private readonly http = inject(HttpClient);
-
-  public ngOnInit(): void {
-    this.http.get(this.url).subscribe((obj: any) => {
-      const arrayResults: any[] = obj.results;
-
-      for (let result of arrayResults) {
-        this.http.get(result.url).subscribe(objDetails => {
-          const pokemon = this.getPokemon(objDetails);
-
-          this.pokemons.push(pokemon);
-        })
-      }
-      
-      const charizard = arrayResults.find(p => p.name == 'charmander');
-      this.theBest = charizard.name;
-    });
-
-  }
-  private getPokemon(obj: any): Pokemon {
-    return {
-      name: this.convertToTitleCase(obj.name),
-      urlSprite: obj.sprites.front_default,
-      types: obj.types.map((x: any) => this.convertToTitleCase(x.type.name))
-    }
-  }
-
-  private convertToTitleCase(text: string): string {
-    if (text.length < 1) return text;
-
-    const newString = text[0].toUpperCase()
-      + text.substring(1).toLowerCase();
-
-    return newString;
-  }
-}
-
-
+export class App  {}
